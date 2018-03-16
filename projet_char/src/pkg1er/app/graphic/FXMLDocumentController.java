5840354ddba9f216eab44826072da269d5d8f338
bmw546 <marc-etienne-pepin@hotmail.com>
2018-03-16 15:41:14 -0400

    Corriger une erreur dans le take textfield

--- projet_char/src/pkg1er/app/graphic/FXMLDocumentController.java
+++ projet_char/src/pkg1er/app/graphic/FXMLDocumentController.java
@@ -5,16 +5,22 @@
  */
 package pkg1er.app.graphic;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  *
@@ -60,10 +66,13 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private ListView equipement;
     
+
     @FXML
     private void search(ActionEvent event) {
-        String temp = text.getText();
+        //String temp = text.getText();
         
+        System.out.println(text.getText());
+        this.setcouleur(text.getText());
     }
     @FXML
     private void back(ActionEvent event) {
